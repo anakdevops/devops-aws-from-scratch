1791221d91311fac59b7e16f192d57a3171dcd61
Anak Devops <anakdevops93@gmail.com>
2024-08-25 14:55:50 +0700

    add

--- s3/main.tf
+++ s3/main.tf
@@ -18,3 +18,11 @@ resource "aws_s3_bucket" "my_bucket" {
   bucket = "data-s3-anakdevops"
   force_destroy = true
 }
+
+# Define S3 bucket object resource for uploading a file
+resource "aws_s3_object" "example_file" {
+  bucket = aws_s3_bucket.my_bucket.bucket
+  key    = "nginx.conf"  # Name of the file in S3
+  source = "nginx.conf"  # Path to your local file
+  acl    = "private"
+}
